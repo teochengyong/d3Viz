49bf17b97fa1f12e0d752beaa2a1e602abee1a9f
Cheng Yong <teochengyong@gmail.com>
2017-06-05 15:46:32 +0800

    Drawing cirles and bars

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@
   ]
   const d3Viz = {}
 
-  const height = 300
-  const width = 300
+  const height = 400
+  const width = 500
   barWith = width / data.length - 1
 
   // const xRange = w.d3.scaleLinear([0, width])
@@ -41,8 +41,65 @@
       .style('height', function(d){ return d*5 + 'px'})
   }
 
+  const drawWithSvg = function () {
+    let dataSet = []
+    for( let i=0; i<25; i++ ) {
+      dataSet.push(Math.round(Math.random() * height))
+    }
+    let barWidth = Math.round( width/ dataSet.length)
+    let chart = w.d3
+      .select('#svgChart')
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('g')
+      .data(dataSet)
+      .enter()
+      .append('rect')
+      .attr('x', function(d, index) {
+        return index * barWidth
+      })
+      .attr('y', function(d){
+        return Math.round(height - d)
+      })
+      .attr('width', barWidth - 10)
+      .attr('height', function(d) {return d})
+      .attr('class', 'bar')
+  }
+
+  const drawCirclesWithSvg = function () {
+    let dataSet = []
+    let colors = ['green', 'blue', 'red', ]
+    for( let i=0; i<5; i++ ) {
+      dataSet.push(Math.random())
+    }
+    let radius = Math.round(width / dataSet.length) / 2
+    let chart = w.d3
+      .select('#svgChart')
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('g')
+      .data(dataSet)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d, index) {
+        return index * radius + 2*radius
+      })
+      .attr('cy', function(d, index){
+        return index * radius + 2*radius
+      })
+      .attr('r', radius)
+      .attr('fill', function(d) {
+        return `rgba(${Math.round(255*d)}, ${Math.round(255*d)}, ${Math.round(255*d)}, ${d})`
+      })
+      .attr('stroke', function(d, index) {
+        return `rgba(${ index / 5 === 1 ? Math.round(255*d) : 128}, ${Math.round(255*d)}, ${ index / 4 === 1 ? Math.round(255*d) : 128}, ${d})`
+      })
+      .attr('stroke-width', '3')             
+  }  
+
   d3Viz.draw = draw
   d3Viz.drawRandom = drawRandom
-
+  d3Viz.drawWithSvg = drawWithSvg
+  d3Viz.drawCirclesWithSvg = drawCirclesWithSvg
   window.d3Viz = d3Viz
 })(window)
