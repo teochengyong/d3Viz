7c1d04f31ad71156b818dcadcf04ac6a3019f40b
chengyong <teochengyong@gmail.com>
2017-06-16 09:03:28 +0800

    Making bar chart more generic, vertical bars with different category.

--- src/modules/barChart.js
+++ src/modules/barChart.js
@@ -1,37 +1,29 @@
 (function (context) {
   const module = context.d3Viz
   const d3 = context.d3
-  const width = 600
+  const width = 200000
   const height = 250
 
-  const getMax = function (array) {
-    let curMax
-    array.forEach(function (val) {
-      if (curMax === undefined) {
-        curMax = val
-      } else if (val > curMax) {
-        curMax = val
-      }
+  const drawBarChart = function (dataset, options) {
+    const y = options.y
+    const x = options.x
+    const yMax = d3.max(dataset, function (data) {
+      return data[y]
     })
-    return curMax
-  }
-
-  const drawBarChart = function (dataset) {
     const yScale = d3.scaleLinear()
-      .domain([0, d3.max(dataset)])
+      .domain([0, yMax])
       .range([0, height])
     const xScale = d3.scaleBand()
       .domain(d3.range(dataset.length))
       .rangeRound([0, width])
       .padding(0.1)
 
-    const maxData = getMax(dataset)
     let svg = context.d3.select('#chart')
       .append('svg')
       .attr('width', width)
       .attr('height', height)
       .attr('id', 'bar')
-      .attr('viewBox', '0 0 600 250')
+      .attr('viewBox', `0 0 ${width} ${height}`)
 
     svg.selectAll('rect')
       .data(dataset)
@@ -41,17 +33,17 @@
         return xScale(index)
       })
       .attr('y', function (d, index) {
-        return height - yScale(d)
+        return height - yScale(d[y])
       })
       .attr('width', function (d, index) {
         return xScale.bandwidth()
       })
       .attr('height', function (d) {
-        return yScale(d) + 'px'
+        return yScale(d[y]) + 'px'
       })
       // drawing relative fill according to max data
       .attr('fill', function (d) {
-        let normalizedRGB = Math.round((d / maxData) * 255)
+        let normalizedRGB = Math.round((d[y] / yMax) * 255)
         return `rgb(0,0, ${normalizedRGB})`
       })
 
@@ -60,34 +52,36 @@
       .enter()
       .append('text')
       .text(function (d) {
-        return d
+        return d[y]
       })
       .attr('x', function (data, index) {
         return xScale(index) + (xScale.bandwidth() / 2)
       })
       .attr('y', function (data, index) {
-        return (height - yScale(data)) + 15
+        return (height - yScale(data[y])) + 15
       })
       .attr('font-family', 'sans-serif')
       .attr('font-size', '11px')
       .attr('fill', 'white')
       .attr('text-anchor', 'middle')
 
-    d3.select('#bar')
-      .on('click', function () {
-        // New values for dataset
-        const dataset = [11, 12, 15, 20, 18, 17, 16, 18, 23, 25,
-          5, 10, 13, 19, 21, 25, 22, 18, 15, 13]
-
-        svg.selectAll('rect')
-          .data(dataset)
-          .attr('y', function (d) {
-            return height - yScale(d)
-          })
-          .attr('height', function (d) {
-            return yScale(d)
-          })
+    svg.selectAll('text')
+      .data(dataset)
+      .enter()
+      .append('text')
+      .text(function (d) {
+        return d[x]
       })
+      .attr('x', function (data, index) {
+        return xScale(index) + (xScale.bandwidth() / 2)
+      })
+      .attr('y', function (data, index) {
+        return height + 15
+      })
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', '11px')
+      .attr('fill', 'black')
+      .attr('text-anchor', 'middle')
   }
   module.drawBarChart = drawBarChart
 })(window)
